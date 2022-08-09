ecddce1a489810d3d263c84de2c64a6ae6e4061a
Abbracx <tankoraphael@gmail.com>
2022-08-09 20:28:25 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Favorites from "./pages/movieList/Favorites";
+import { Routes, Route } from "react-router-dom";
+import MainScreen from "./pages/movieList/MainScreen";
+import FormLogin from "./components/form/FormLogin";
+import UserProvider from "./stateContext/StateProvider";
+import WatchList from "./pages/movieList/WatchList";
+import ViewSingleMovieDetails from "./pages/header/ViewSingleMovieDetails";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Nav from "./components/navbar/Nav";
+import PageNotFound from "./pages/pageNotFound/PageNotFound";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+
+      <UserProvider>
+        <Routes>
+          <Route exact path="/" element={<ProtectedRoute />}>
+            <Route exact path="/" element={<MainScreen />} />
+          </Route>
+          <Route exact path="/favorites" element={<ProtectedRoute />}>
+            <Route exact path="/favorites" element={<Favorites />} />
+          </Route>
+          <Route exact path="/favorites" element={<ProtectedRoute />}>
+            <Route exact path="/favorites" element={<Favorites />} />
+            <Route path="/favorites/:id" element={<ViewSingleMovieDetails />} />
+          </Route>
+          <Route exact path="/watchlist" element={<ProtectedRoute />}>
+            <Route exact path="/watchlist" element={<WatchList />} />
+          </Route>
+          <Route path="/login" element={<FormLogin />} />
+          <Route path="/*" element={<PageNotFound/>} />
+          
+        </Routes>
+      </UserProvider>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { StateProvider } from './stateContext/StateProvider';
 import reportWebVitals from './reportWebVitals';
+import { initialState } from './stateContext/reducer';
+import reducer from './stateContext/reducer';
+import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+
+        <StateProvider initialState={initialState} reducer={reducer}>
+             <App />
+        </StateProvider>
+     
+    </BrowserRouter>
+
+  
+
+ 
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
